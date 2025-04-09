0861717da5f85525f1f6afea99cf4650301adcac
Zach Holloway <dev@a2ztech.io>
2025-04-08 22:26:03 -0400

    player added and movement added to gameloop

--- main.py
+++ main.py
@@ -1,23 +1,40 @@
-
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from constants import *
-
 import pygame
+from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    # pygame.display.set_caption(f"Starting Asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
-
-    running = True
-    print(f"Starting Asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
-    while running:
+    clock = pygame.time.Clock()
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable) #pyright: ignore
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                return
 
+        # player.update(dt)
+        # player.draw(screen)
+        updatable.update(dt)
+        screen.fill("black")
+        
+        for sprite in drawable:
+            sprite.draw(screen)
+      
+            
         pygame.display.flip()
 
-    pygame.quit()
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
+
 if __name__ == "__main__":
     main()
+
