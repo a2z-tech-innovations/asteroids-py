4904a91ddddf26a951b6de44324aaf876cd11b19
Zach Holloway <dev@a2ztech.io>
2025-04-10 12:36:46 -0400

    asteroids spawning, added shot and bullets. Functioning as intended

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
-from constants import *
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 
 def main():
@@ -10,20 +13,31 @@ def main():
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group() 
     
     Player.containers = (updatable, drawable) #pyright: ignore
+    Asteroid.containers = (asteroids,updatable,drawable) #pyright: ignore
+    AsteroidField.containers = (updatable) #pyright: ignore
+    Shot.containers = (shots,updatable,drawable) #pyright: ignore
     
+    asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        # player.update(dt)
-        # player.draw(screen)
+       
         updatable.update(dt)
         screen.fill("black")
+        
+        for asteroid in asteroids:
+            if player.collision(asteroid):
+                print("Game Over!")
+                return
         
         for sprite in drawable:
             sprite.draw(screen)
